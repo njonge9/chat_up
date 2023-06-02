1ddb7982bd0377962ed3ff6acfd13985012dff87
njonge9 <frednjonge9@gmail.com>
2023-06-02 07:11:28 +0300

    use DRY method

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
