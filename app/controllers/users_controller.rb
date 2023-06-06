29a4ea2d6fd3af6d3bd46b7936d1f8181be28e53
njonge9 <frednjonge9@gmail.com>
2023-06-06 22:11:37 +0300

    A working form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create 
+    @user = User.new(params[:user])
+
+    if @user.save
+      # handle a successful save
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
 end
